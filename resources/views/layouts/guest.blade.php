2f20ab7a407a2d870d6f4d8f641b8f6b7e040eae
Necks <n.ubatay.556947@umindanao.edu.ph>
2026-05-10 22:57:59 +0800

    Third Commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'En.cafe') }}</title>
 
         <!-- Fonts -->
         <link rel="icon" type="image/png" href="{{ asset('images/logos.png') }}">
